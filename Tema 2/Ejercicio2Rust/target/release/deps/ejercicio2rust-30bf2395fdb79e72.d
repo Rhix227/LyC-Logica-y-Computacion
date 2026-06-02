cd5b38ae269c7a1d60ee3c386a149d68b069bfff
Yvanna Bravo <yva.bravo16@gmail.com>
2026-06-02 19:34:03 -0400

    Limpieza del proyecto: se elimina la carpeta target y el .gitignore vacío del repositorio

--- Tema 2/Ejercicio2Rust/target/release/deps/ejercicio2rust-30bf2395fdb79e72.d
+++
@@ -1,5 +0,0 @@
-C:\Users\IVANNA\LyC-Logica-y-Computacion\Tema 2\Ejercicio2Rust\target\release\deps\ejercicio2rust-30bf2395fdb79e72.d: src\main.rs
-
-C:\Users\IVANNA\LyC-Logica-y-Computacion\Tema 2\Ejercicio2Rust\target\release\deps\ejercicio2rust-30bf2395fdb79e72.exe: src\main.rs
-
-src\main.rs:
--- Tema 2/Ejercicio2Rust/target/release/ejercicio2rust.d
+++
@@ -1,1 +0,0 @@
-C:\Users\IVANNA\LyC-Logica-y-Computacion\Tema\ 2\Ejercicio2Rust\target\release\ejercicio2rust.exe: C:\Users\IVANNA\LyC-Logica-y-Computacion\Tema\ 2\Ejercicio2Rust\src\main.rs
